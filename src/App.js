f4e0f2f2f72703a38aa78d249f178f5875924440
Victor Sarov <victor.sarov@rentalmatics.com>
2023-07-21 20:57:03 +0100

    all good

--- src/App.js
+++ src/App.js
@@ -2,26 +2,11 @@ import React from "react";
 import "./App.css";
 
 // components
-import BasicTable from "./components/BasicTable";
-import SortingTable from "./components/SortingTable";
-import GlobalFiltering from "./components/GlobalFiltering";
-import ColumnFiltering from "./components/ColumnFiltering";
-import PaginationTable from "./components/PaginationTable";
-import SelectingRow from "./components/SelectingRow";
-import ColumnOrdering from "./components/ColumnOrdering";
-import ColumnHiding from "./components/ColumnHiding";
 
 function App() {
   return (
     <div>
-      {/* <BasicTable /> */}
-      {/* <SortingTable /> */}
-      {/* <GlobalFiltering /> */}
-      {/* <ColumnFiltering /> */}
-      {/* <PaginationTable /> */}
-      {/* <SelectingRow /> */}
-      {/* <ColumnOrdering /> */}
-      <ColumnHiding />
+      <h1>Hello World</h1>
     </div>
   );
 }
